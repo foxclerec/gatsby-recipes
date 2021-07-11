8cc9da558f195474f0c274faeb1c7a77010af778
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-11 12:48:54 +0200

    Navigation added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>Simple Recipes</span>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 // Styles
+import "normalize.css"
 import "../assets/css/main.css"
 
 const Layout = ({ children }) => {
